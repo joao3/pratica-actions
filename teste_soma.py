35efb6add9384bd89fa365d3fe487c235b1afac9
Joao Otavio <joao0otavio3@gmail.com>
2023-05-11 23:57:22 -0300

    adiciona multiplicacao com erro

--- teste_soma.py
+++ teste_soma.py
@@ -5,7 +5,6 @@ class TesteDaSoma(unittest.TestCase):
 
     def test(self):
         self.assertEqual(soma(1, 2), 3)
-        self.assertEqual(soma(-1, 1), 0)
 
 if __name__ == '__main__':
     unittest.main()
